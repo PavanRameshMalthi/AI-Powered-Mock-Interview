bea9c88dfb2dc0f351a1a92ad1426ea4658551a4
PavanRameshMalthi <pavanrameshmalthi886@gmail.com>
2026-06-16 20:33:57 +0530

    Add package.json with axios and react-router-dom dependencies

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Landing from "../pages/Landing/Landing";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Dashboard from "../pages/Dashboard/Dashboard";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
