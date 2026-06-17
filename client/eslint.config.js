ac39f1fbf2111a25799b9878e4b3c8cdb5b0b61c
PavanRameshMalthi <pavanrameshmalthi886@gmail.com>
2026-06-17 23:53:15 +0530

    fix: update ESLint configuration to ignore coverage directory and add test file support

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -15,6 +15,17 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+  },
+  {
+    files: ['**/*.test.{js,jsx}', 'src/test/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
