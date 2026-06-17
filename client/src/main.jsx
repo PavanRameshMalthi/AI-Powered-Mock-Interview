0d60c3e305c9fc77f20cba603dee542a6f3f2344
PavanRameshMalthi <pavanrameshmalthi886@gmail.com>
2026-06-17 21:55:29 +0530

    fix: correct JSX formatting in main component rendering

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,15 @@
-```jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import { Toaster } from "react-hot-toast";
 
-<React.StrictMode>
-  <App />
-  <Toaster
-    position="top-right"
-    reverseOrder={false}
-  />
-</React.StrictMode>
-```
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    <Toaster
+      position="top-right"
+      reverseOrder={false}
+    />
+  </React.StrictMode>
+);
